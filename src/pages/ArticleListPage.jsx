6f823335a5ed6b46345a0e87da40c45b1bd0094d
kaly <kalimamadou@gmail.com>
2020-05-17 16:16:28 -0700

    create a link from article list to article page using url parameters to pass information

--- src/pages/ArticleListPage.jsx
+++ src/pages/ArticleListPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import articles from "./article-content";
 
 const ArticleListPage = () => {
-  return <div>Article List Page</div>;
+  return (
+    <div>
+      <h1>Articles</h1>
+      {articles.map((article, key) => (
+        <Link
+          className="article-list-item"
+          key={key}
+          to={`/article/${article.name}`}
+        >
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}...</p>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default ArticleListPage;
--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import articles from "./article-content";
 
-const ArticlePage = () => {
-  return <div>Article Page</div>;
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+  const articleContent = articles.find((article) => article.name === name);
+  if (!articleContent) return <h1>Article does not exist!</h1>;
+  return (
+    <div>
+      <h1>{articleContent.title}</h1>
+      {articleContent.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+    </div>
+  );
 };
 
 export default ArticlePage;
