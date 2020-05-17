88befb933035fb9f066bdae5b3f4dbec836716f6
kaly <kalimamadou@gmail.com>
2020-05-17 16:28:41 -0700

    Separate ArticleListPage to a component and added access to article List component on article page

--- src/pages/ArticleListPage.jsx
+++ src/pages/ArticleListPage.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import articles from "./article-content";
+import ArticlesList from "../components/article-list/article-list.component";
 
 const ArticleListPage = () => {
   return (
     <div>
       <h1>Articles</h1>
-      {articles.map((article, key) => (
-        <Link
-          className="article-list-item"
-          key={key}
-          to={`/article/${article.name}`}
-        >
-          <h3>{article.title}</h3>
-          <p>{article.content[0].substring(0, 150)}...</p>
-        </Link>
-      ))}
+      <ArticlesList articles={articles} />
     </div>
   );
 };
--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import articles from "./article-content";
+import ArticlesList from "../components/article-list/article-list.component";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const articleContent = articles.find((article) => article.name === name);
   if (!articleContent) return <h1>Article does not exist!</h1>;
+  const otherArticles = articles.filter((article) => article.name !== name);
   return (
     <div>
       <h1>{articleContent.title}</h1>
       {articleContent.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
     </div>
   );
 };
