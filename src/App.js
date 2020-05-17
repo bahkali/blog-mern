40025989d98eec60a686f7d87ccb84018605d6e2
kaly <kalimamadou@gmail.com>
2020-05-17 14:02:07 -0700

    Added the navbar component and implement to all pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import "./App.css";
 import homePage from "./pages/homePage";
+import ArticlePage from "./pages/ArticlePage";
+import AboutPage from "./pages/AboutPage";
+import ArticleListPage from "./pages/ArticleListPage";
+import NavBar from "./components/navbar/navbar.component";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Route path="/" component={homePage} exact />
+          <NavBar />
+          <div id="page-body">
+            <Route path="/" component={homePage} exact />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/article-list" component={ArticleListPage} />
+          </div>
         </div>
       </BrowserRouter>
     );
